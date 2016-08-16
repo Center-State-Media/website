e4d6852f80f9b1277491d59865536f407d5f1cea
Andrew Vye <a.vye26@gmail.com>
2016-08-16 15:21:09 -0700

    (feat) update style sheet and app.js

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,24 +1,37 @@
-angular.module("app", [
+angular.module('app', [
   "timer",
   "app.auth",
   "app.race",
-  "ngRoute"
+  "ngRoute",
+  'ui.bootstrap'
   ])
 
-.config(function($routeProvider){
-  $routeProvider
-      .when('/signin', {
-        templateUrl: '../auth/signin.html',
-        controller: 'AuthController'
-      })
-      // The raceView address will have the user and room id
-      // These ids are used for the socket instantiateUser event
-      .when('/raceView/:roomId/:userId', {
-        templateUrl: '../race/race.html',
-        controller: 'raceController'
-      })
-      // Default back to /signin page
-      .otherwise({
-        redirectTo: '/signin'
-      });
-});
+.controller('mainController', function($scope) {
+  scope.bigData = {};
+
+  $scope.bigData.breakfast = false;
+  $scope.bigData.lunch = false;
+  $scope.bigData.dinner = false;
+
+  // COLLAPSE =====================
+  $scope.isCollapsed = false;
+})
+
+
+// .config(function($routeProvider){
+//   $routeProvider
+//       .when('/signin', {
+//         templateUrl: '../auth/signin.html',
+//         controller: 'AuthController'
+//       })
+//       // The raceView address will have the user and room id
+//       // These ids are used for the socket instantiateUser event
+//       .when('/raceView/:roomId/:userId', {
+//         templateUrl: '../race/race.html',
+//         controller: 'raceController'
+//       })
+//       // Default back to /signin page
+//       .otherwise({
+//         redirectTo: '/signin'
+//       });
+// });
