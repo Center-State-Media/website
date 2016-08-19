3deb986bc8b5212836880a8406638fed6897e758
Andrew Vye <a.vye26@gmail.com>
2016-08-18 18:09:29 -0700

    (feat) Fix scrollspy with offsets

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -2,16 +2,22 @@ angular.module('home', [
   'duScroll'
 ])
 
-.controller('homeController', function($scope, $rootScope) {
+.controller('homeController', function($scope, $rootScope, $document) {
 // This isn't really doing anything currently
-  $rootScope.$on('duScrollspy:becameActive', function($event, $element, $target){
-    console.log('the event ', $event);
-    console.log('the element ', $element);
-    console.log('the target ', $target);
-      //Automaticly update location
-      var hash = $element.prop('hash');
-      if (hash) {
-        history.replaceState(null, null, hash);
-      }
-    });
+  $scope.sidebarScrollTo = function(location) {
+    console.log('loc ', location);
+    var element = angular.element(document.getElementById(location));
+    console.log('ele ', element);
+    $document.scrollToElementAnimated(element, 50);
+  }
+  // $rootScope.$on('duScrollspy:becameActive', function($event, $element, $target){
+  //   console.log('the event ', $event);
+  //   console.log('the element ', $element);
+  //   console.log('the target ', $target);
+  //     //Automaticly update location
+  //     var hash = $element.prop('hash');
+  //     if (hash) {
+  //       history.replaceState(null, null, hash);
+  //     }
+  //   });
 })
