6dc38e7611177504f60fa62992e842b96655236f
Andrew Vye <a.vye26@gmail.com>
2016-10-06 15:09:03 -0700

    (feat) Update contact form to have required fields to prevent spam submissions

--- client/src/contact/contact.js
+++ client/src/contact/contact.js
@@ -2,15 +2,24 @@ angular.module('contact', [
 
 ])
 
-.controller('contactController', function($scope, $uibModalInstance) {
+.controller('contactController', function($scope, $http) {
   $scope.userContact = {};
 
-  $scope.closeContactForm = function() {
-    $uibModalInstance.dismiss('cancel');
-  }
 
   $scope.submitContactForm = function() {
-
-    $uibModalInstance.dismiss('cancel');
+    console.log('the ', $scope.userContact);
+    $http.get('/contactSubmit', {
+      params: {
+        contactName: $scope.userContact.name,
+        contactEmail: $scope.userContact.email,
+        contactPhone: $scope.userContact.phone,
+        contactDescription: $scope.userContact.interest
+      }
+    })
+    .then(function(resp) {
+      console.log(resp);
+    }, function(error) {
+      console.log(resp);
+    })
   }
 })
