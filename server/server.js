b9a7889375b29312b87aefc71523ba2c90264fb1
Andrew Vye <a.vye26@gmail.com>
2016-10-06 15:12:01 -0700

    (feat) Get mailgun set up to handle contact submissions

--- server/server.js
+++ server/server.js
@@ -1,71 +1,46 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
+// var mailgun = require('mailgun-js')({apiKey: process.env.mailgunApiKey, domain: process.env.mailgunDomain});
 
 var app = express();
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// authInfo.user = process.env.user
+// authInfo.mailgunApiKey = process.env.mailgunApiKey
+// authInfo.mailgunDomain = process.env.mailgunDomain
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(helmet());
 
 // Set our port
 var port = process.env.PORT || 3000;
 
-// Serve up our static files (it has to serve up a directory)
+// Serve up our static files
 app.use(express.static(__dirname+'/../client'));
 
-// Get our router set up
-var router = express.Router();
-
-// Basic router to run first
-router.use(function(req, res, next) {
-    console.log('A request has been sent.');
-    next(); // sends us to the next route
-});
-
-// // router.route('/animals')
-//   .get(function(req, res) {
-//     Animal.find({}).exec(function(err, animals) {
-//       if (err) {
-//         console.log('Failed to round up the animals');
-//       } else {
-//         res.json(animals);
-//       }
-//     });
-//   })
-//
-//   .post(function(req, res) {
-//     // Assuming that we don't
-//     var animal = new Animal({
-//       name: req.body.name,
-//       type: req.body.type,
-//       diet: req.body.diet,
-//       lifespan: req.body.lifespan,
-//       size: req.body.size,
-//       weight: req.body.weight,
-//       image: req.body.image,
-//     });
-//
-//     animal.save(function(err) {
-//       if (err) {
-//         res.send(err);
-//       }
-//
-//       res.json({message: 'Animal created!'});
-//     });
-//   });
-
-// All of our routes will be prefixed with /api
-app.use('/api', router);
+app.get('/contactSubmit', function (req, res) {
+  console.log(req.query);
+  // Create the email mailgun will send
+  // var data = {
+  //   from: 'Contact Request <me@samples.mailgun.org>',
+  //   to: process.env.user,
+  //   subject: 'New contact request from ' + req.query.contactName + ' at ' + req.query.contactCompany,
+  //   text: 'Email me at ' + req.query.contactEmail + '\n' + 'I am interested in: ' + req.query.contactDescription
+  // };
+  //
+  // mailgun.messages().send(data, (error, body) => {
+  //   if (error) {
+  //     res.end('Message Error')
+  //   } else {
+  //     res.end('Message Sent');
+  //   }
+  // });
+})
 
 
 // Start the server
 app.listen(port);
 console.log('Server started and listening on ' + port);
-
-
-// Serve up the static file
-// Will need to make this path relative with __dirname + '/client'
-// app.use(express.static('../Client'));
 
 module.exports = app;
