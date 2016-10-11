47842b427a04aed4b64876668ff18db975538c5c
Andrew Vye <a.vye26@gmail.com>
2016-10-11 13:17:40 -0700

    (feat) Begin setting up database

--- server/database/config.js
+++ server/database/config.js
@@ -1,13 +1,3 @@
-var mongoose = require('mongoose');
+var Sequelize = require('sequelize'):
 
-mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/nameThatAnimal';
-
-mongoose.connect(mongoURI);
-var db=mongoose.connection;
-
-db.on('error', console.error);
-db.once('open', function() {
-  console.log('Time for some Animals!');
-});
-
-module.exports = db;
+var sequelize = new Sequelize("users", "root", "")
---
+++ server/database/models/users.js
@@ -0,0 +1,29 @@
+var Sequelize = require('sequelize');
+var sequelize = require('../config.js');
+var bcrypt = require('bcrypt');
+
+var User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+})
