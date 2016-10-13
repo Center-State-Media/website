f264898d3842f98dd472f65d58ae60364b1ff346
Andrew Vye <a.vye26@gmail.com>
2016-10-12 18:58:17 -0700

    (feat) Alter database settings to remove port callout

--- server/database/config.js
+++ server/database/config.js
@@ -3,10 +3,12 @@ var url = process.env.DATABASE_URL || 'postgres://localhost:5432/test';
 
 var sequelize = new Sequelize(url, {
   dialect: 'postgres',
-  protocol: 'postgres',
-  port: match[4],
-  host: match[3],
-  logging: true
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
