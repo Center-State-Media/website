2ce6214eade1ab94ff195e3bc83d5dee19a00790
Andrew Vye <a.vye26@gmail.com>
2016-10-12 19:02:23 -0700

    (fix) Remove colon and replace with semicolon

--- server/database/config.js
+++ server/database/config.js
@@ -1,4 +1,4 @@
-var Sequelize = require('sequelize'):
+var Sequelize = require('sequelize');
 var url = process.env.DATABASE_URL || 'postgres://localhost:5432/test';
 
 var sequelize = new Sequelize(url, {
